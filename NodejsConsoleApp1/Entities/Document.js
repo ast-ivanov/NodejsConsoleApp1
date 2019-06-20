72c3ff080b5a68d54b834077e692a7687b65a8fc
Артур Иванов <arch.step.inc@gmail.com>
2019-06-20 14:56:30 +0800

    Delete ignored files
    * Delete app.js.map
    
    * Delete app.js
    
    * Delete BaseEntity.js
    
    * Delete BaseEntity.js.map
    
    * Delete DeletedEntity.js
    
    * Delete DeletedEntity.js.map
    
    * Delete Document.js
    
    * Delete Document.js.map
    
    * Delete Operator.js
    
    * Delete Operator.js.map
    
    * Delete DocumentBuilder.js
    
    * Delete DocumentBuilder.js.map

--- NodejsConsoleApp1/Entities/Document.js
+++
@@ -1,8 +0,0 @@
-class Document extends DeletedEntity {
-    constructor(id, number, author) {
-        super(id);
-        this.number = number;
-        this.author = author;
-    }
-}
-//# sourceMappingURL=Document.js.map
